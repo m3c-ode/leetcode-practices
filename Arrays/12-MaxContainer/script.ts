56157d8b5323231f8a3163fd92dd6cdd9ae27445
maxime m3.code <maxime.marechalmccoy@gmail.com>
2024-01-13 14:16:00 -0800

    readme and array-12

---
+++ Arrays/12-MaxContainer/script.ts
@@ -0,0 +1,26 @@
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container. */
+
+function maxArea(heights: number[]): number {
+  let maxArea = 0;
+  let i = 0;
+  let j = heights.length - 1;
+  while (i < j) {
+    let width = j - i;
+    let currentArea = Math.min(heights[i], heights[j]) * width;
+    if (currentArea > maxArea) maxArea = currentArea;
+    if (heights[i] < heights[j]) i++;
+    else j--;
+  }
+  return maxArea;
+}
+
+const heights = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+console.log(maxArea(heights));
